9ae84dc31c0e72d99a44c54b389b1e980d77b8e7
Adam Vessey <adam-vessey@users.noreply.github.com>
2015-12-02 17:49:51 -0400

    Yep.

--- tests/scripts/travis_setup.sh
+++ tests/scripts/travis_setup.sh
@@ -1,44 +1,13 @@
 #!/bin/bash
 
+# Drush installation.
 cd $HOME
 composer self-update
 composer global require 'drush/drush' 'squizlabs/php_codesniffer' 'sebastian/phpcpd=*'
 # Because we can't add to the PATH here and this file is used in many repos,
 # let's just throw symlinks into a directory already on the PATH.
 echo linking && find $HOME/.composer/vendor/bin -executable \! -type d -exec sudo ln -s {}  /usr/local/sbin/ \;
-
-# Database creation and priveleges.
-mysql -u root -e 'create database drupal;'
-mysql -u root -e "create database fedora;"
-mysql -u root -e "GRANT ALL PRIVILEGES ON fedora.* To 'fedora'@'localhost' IDENTIFIED BY 'fedora';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON drupal.* To 'drupal'@'localhost' IDENTIFIED BY 'drupal';"
-
-# Java 8, if needed.
-if [ $FEDORA_VERSION = "3.8.1" ]; then
-  sudo add-apt-repository -y ppa:webupd8team/java
-  sudo apt-get update
-  sudo apt-get install -y oracle-java8-installer oracle-java8-set-default
-  sudo update-java-alternatives -s java-8-oracle
-  export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-fi
-
-# Islandora Tomcat installation.
-cd $HOME
-git clone git://github.com/Islandora/tuque.git
-wget http://alpha.library.yorku.ca/islandora_tomcat.$FEDORA_VERSION.tar.gz
-tar xf islandora_tomcat.$FEDORA_VERSION.tar.gz
-cd islandora_tomcat
-export CATALINA_HOME='.'
-export JAVA_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=512m -XX:+CMSClassUnloadingEnabled -Djavax.net.ssl.trustStore=$CATALINA_HOME/fedora/server/truststore -Djavax.net.ssl.trustStorePassword=tomcat"
-# TODO: roll a Fedora 3.8.1 islandora_tomcat that doesn't require a rebuild.
-if [ $FEDORA_VERSION = "3.8.1" ]; then
-  export FEDORA_HOME=fedora
-  ./fedora/server/bin/fedora-rebuild.sh -r org.fcrepo.server.utilities.rebuild.SQLRebuilder
-fi
-./bin/startup.sh
-
-# Drush installation.
-cd $HOME
+sudo apt-get install php5-mysql
 
 # Drupal installation.
 echo dling drupal && drush dl --yes drupal
@@ -72,4 +41,34 @@ drush en --user=1 --yes islandora
 drush cc all
 # The shebang in this file is a bogeyman that is haunting the web test cases.
 rm /home/travis/.phpenv/rbenv.d/exec/hhvm-switcher.bash
+
+# Database creation and priveleges.
+mysql -u root -e 'create database drupal;'
+mysql -u root -e "create database fedora;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON fedora.* To 'fedora'@'localhost' IDENTIFIED BY 'fedora';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON drupal.* To 'drupal'@'localhost' IDENTIFIED BY 'drupal';"
+
+# Java 8, if needed.
+if [ $FEDORA_VERSION = "3.8.1" ]; then
+  sudo add-apt-repository -y ppa:webupd8team/java
+  sudo apt-get update
+  sudo apt-get install -y oracle-java8-installer oracle-java8-set-default
+  sudo update-java-alternatives -s java-8-oracle
+  export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+fi
+
+# Islandora Tomcat installation.
+cd $HOME
+git clone git://github.com/Islandora/tuque.git
+wget http://alpha.library.yorku.ca/islandora_tomcat.$FEDORA_VERSION.tar.gz
+tar xf islandora_tomcat.$FEDORA_VERSION.tar.gz
+cd islandora_tomcat
+export CATALINA_HOME='.'
+export JAVA_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=512m -XX:+CMSClassUnloadingEnabled -Djavax.net.ssl.trustStore=$CATALINA_HOME/fedora/server/truststore -Djavax.net.ssl.trustStorePassword=tomcat"
+# TODO: roll a Fedora 3.8.1 islandora_tomcat that doesn't require a rebuild.
+if [ $FEDORA_VERSION = "3.8.1" ]; then
+  export FEDORA_HOME=fedora
+  ./fedora/server/bin/fedora-rebuild.sh -r org.fcrepo.server.utilities.rebuild.SQLRebuilder
+fi
+./bin/startup.sh
 sleep 20
