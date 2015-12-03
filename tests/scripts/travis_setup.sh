5ab29868934ccd2c4928b381a5d2d5e7700e36d3
Adam Vessey <adam-vessey@users.noreply.github.com>
2015-12-03 15:52:27 -0400

    New drush.

--- tests/scripts/travis_setup.sh
+++ tests/scripts/travis_setup.sh
@@ -3,7 +3,7 @@
 # Drush installation.
 cd $HOME
 composer self-update
-composer global require 'drush/drush' 'squizlabs/php_codesniffer' 'sebastian/phpcpd=*'
+composer global require 'drush/drush:^8.0' 'squizlabs/php_codesniffer' 'sebastian/phpcpd=*'
 # Because we can't add to the PATH here and this file is used in many repos,
 # let's just throw symlinks into a directory already on the PATH.
 echo linking && find $HOME/.composer/vendor/bin -executable \! -type d -exec sudo ln -s {}  /usr/local/sbin/ \;
