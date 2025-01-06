6feb63badf20a30e2b2a61f16e4dc67619ca8369
Adam Vessey <adam@discoverygarden.ca>
2025-01-06 12:10:14 -0400

    Pull apart the separate header manipulations.
    Also, attempt to update tokens to add the full event info.

--- src/EventSubscriber/StompHeaderEventSubscriber.php
+++ src/EventSubscriber/StompHeaderEventSubscriber.php
@@ -8,6 +8,7 @@ use Drupal\islandora\Event\StompHeaderEventInterface;
 use Drupal\jwt\Authentication\Event\JwtAuthEvents;
 use Drupal\jwt\Authentication\Event\JwtAuthGenerateEvent;
 use Drupal\jwt\JsonWebToken\JsonWebToken;
+use Drupal\jwt\Transcoder\JwtDecodeException;
 use Drupal\jwt\Transcoder\JwtTranscoderInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -32,7 +33,10 @@ class StompHeaderEventSubscriber implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents() {
     return [
-      StompHeaderEventInterface::EVENT_NAME => ['baseHeaders', -100],
+      StompHeaderEventInterface::EVENT_NAME => [
+        ['baseHeaders', -100],
+        ['persistentMessages', -1000],
+      ],
     ];
   }
 
@@ -43,24 +47,41 @@ class StompHeaderEventSubscriber implements EventSubscriberInterface {
     $headers = $stomp_event->getHeaders();
 
     if (!$headers->has('Authorization')) {
+      // No header, generate one.
       $token = $this->generateToken($stomp_event);
       if (empty($token)) {
         // JWT does not seem to be properly configured.
         // phpcs:ignore DrupalPractice.General.ExceptionT.ExceptionT
         throw new StompHeaderEventException($this->t('Error getting JWT token for message. Check JWT Configuration.'));
       }
-      else {
-        $headers->set('Authorization', "Bearer $token");
+
+      $headers->set('Authorization', "Bearer {$token}");
+      return;
+    }
+
+    // Get the token, and re-create with additional info.
+    $header_value = $headers->get('Authorization');
+    if (!str_starts_with($header_value, 'Bearer ')) {
+      return;
+    }
+
+    $token_string = explode(' ', $header_value, 2)[1] ?: '';
+    if (!$token_string) {
+      return;
+    }
+    try {
+      $jwt = $this->transcoder->decode($token_string);
+      $this->setEventClaims($jwt, $stomp_event);
+      $updated_token_string = $this->transcoder->encode($jwt);
+      if (!$updated_token_string) {
+        throw new StompHeaderEventException('Error getting updated JWT. Check JWT configuration.');
       }
+      $headers->set('Authorization', "Bearer {$updated_token_string}");
     }
-
-    // In ActiveMQ, STOMP messages are not persistent by default; however, we
-    // would like them to persist, by default... make it so, unless something
-    // else has already set the header.
-    if (!$headers->has('persistent')) {
-      $headers->set('persistent', 'true');
+    catch (JwtDecodeException) {
+      // No-op; failed to decode the token, presumably it was generated/provided
+      // by some other means.
     }
-
   }
 
   /**
@@ -90,9 +111,23 @@ class StompHeaderEventSubscriber implements EventSubscriberInterface {
    *   The event for which we are to add the claim.
    */
   protected function setEventClaims(JsonWebToken $jwt, StompHeaderEventInterface $event) : void {
-    // Set the args into the token instead of depending on headers, so we can
-    // be sure things were not mangled between here and Crayfish.
+    // Set the args into the token to not depend on headers, so we are sure
+    // things were not mangled/manipulated between Islandora and Crayfish.
     $jwt->setClaim('x-islandora-event-data', $event->getData());
+  }
+
+  /**
+   * Event callback; set messages to be persistent by default.
+   */
+  public function persistentMessages(StompHeaderEventInterface $event) : void {
+    $headers = $event->getHeaders();
+
+    // In ActiveMQ, STOMP messages are not persistent by default; however, we
+    // would like them to persist, by default... make it so, unless something
+    // else has already set the header.
+    if (!$headers->has('persistent')) {
+      $headers->set('persistent', 'true');
+    }
   }
 
 }
